97e6aad582a3d8691af49d9975f407b95db974fd
Shicheng Guo <Shicheng.Guo@hotmail.com>
2019-10-13 00:25:26 -0500

    Update bismark.sh

--- bin/bismark.sh
+++ bin/bismark.sh
@@ -1,6 +1,6 @@
+cd ~/hpc/methylation/clep/wgbs/test/data
 zcat ../../SRR9888304_1.fastq.gz | head -n 500000 | gzip -c > SRR9888304_1.fastq.gz
 zcat ../../SRR9888304_2.fastq.gz | head -n 500000 | gzip -c > SRR9888304_2.fastq.gz
-cd /gpfs/home/guosa/hpc/methylation/clep/wgbs/test/data
 wget https://raw.githubusercontent.com/Shicheng-Guo/SmartMeth/master/bin/smartbismark.pl -O smartbismark.pl
 perl smartbismark.pl --input SraRunTable.txt --genome hg19 --server MCRI --queue shortq --submit submit
 
