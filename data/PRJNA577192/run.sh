172c288c8dce26fd1763856ac9e6c3f3a7145cde
Shicheng Guo <Shicheng.Guo@hotmail.com>
2019-10-22 10:26:41 -0500

    Update run.sh

--- data/PRJNA577192/run.sh
+++ data/PRJNA577192/run.sh
@@ -1,1 +1,8 @@
+cd /gpfs/home/guosa/PRJNA577192/srr
+
+fastq-dump --skip-technical --split-files -X 500000 --gzip SRR10290287
+
+wget https://raw.githubusercontent.com/Shicheng-Guo/SmartMeth/master/bin/smartbismark.pl -O smartbismark.pl
+perl smartbismark.pl --input SraRunTable.txt --genome hg19 --server MCRI --queue shortq --submit submit
+
 
